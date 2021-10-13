1e626bc6045650e5ceb359a863ac5fbbf3fc6eee
StephenBrooks220413 <krammer439298@gmail.com>
2021-10-13 15:45:32 -0500

    update to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Col, Container, Row} from "react-bootstrap";
 import '../css/home.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Home(){
     return (
