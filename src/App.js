e1ff732271e83c62497f6e1dc930f9b114e401a2
StephenBrooks220413 <krammer439298@gmail.com>
2021-10-13 15:40:54 -0500

    changes to home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+//pages
+import Home from './pages/Home';
+
+//style
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <Navbar fixed="top" bg="light" expand="lg">
+            <Container>
+              <Navbar.Brand href="#home">AGENCIES</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="#home">
+                    <Link className={"nav-link"} to={"/"}>Home</Link>
+                  </Nav.Link>
+                  <Nav.Link href="#link">
+                    <Link className={"nav-link"} to={"/about"}>About</Link>
+                  </Nav.Link>
+                  <NavDropdown className={"nav-link"} title="Insurance" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Auto</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Home/Renter</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Life/Health</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Existing Client</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
-export default App;
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {Col, Container, Row} from "react-bootstrap";
+import '../css/home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function Home(){
+    return (
+        <>
+            {/*main intro*/}
+            <div className={"main-home-img text-white"}>
+                <Container>
+                    <h1 className={"display-2"}>Auto Savings</h1>
+                    <p class={"lead"}>For Savings on the road, go with Pedro</p>
+                    <a href="mailto:pdominguez1@yahoo.com" className={"btn btn-primary btn-lg"}>MESSAGE ME</a>
+                </Container>
+            </div>
+            {/*main intro*/}
+
+            {/* content */}
+            <Container className={"text-center space"}>
+                <Row>
+                    <Col className={"text"} xs sm="12" md="4" lg="4">
+                        When you are in need of a covered auto policy,
+                        and want coverage to cover it all,
+                    </Col>
+                    <Col xs sm="12" md="4" lg="4">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTddlQzSeZg4O_VANJOBu0GoQL8gYzFK4xoCQ&usqp=CAU"
+                             className={"img-fluid"}
+                             alt="car"/>
+                    </Col>
+                    <Col className={"text"} xs sm="12" md="4" lg="4">
+                        Let us cover that for you and find what savings or
+                        discounts you may be elgible for.
+                    </Col>
+                </Row>
+            </Container>
+            {/* content */}
+
+            {/* content */}
+            <Container className={"space"}>
+                <h1 className={"text-center"}>About Insurance Policies</h1>
+                <hr className={"hr"}/>
+                <br/>
+                <Row>
+                    <Col className={"text"} xs sm="12" md="12" lg="6">
+                        When you are in need of a insurance policy,
+                        and want coverage to cover it all, get q quote
+                        from me. I offer low rates for your every day,
+                        especially if you're a senior. I can assist with
+                        your home, auto, life and health.
+                        <br/>
+                        <a href="mailto:pdominguez1@yahoo.com" className={"btn btn-primary"}>ASK NOW</a>
+                    </Col>
+                    <Col xs sm="12" md="12" lg="6">
+                        <img src="https://media.istockphoto.com/photos/business-woman-examining-insurance-policy-picture-id1180387704?b=1&k=20&m=1180387704&s=170667a&w=0&h=4LR27VcXCXVhfH9jvUFCN9_nnU8CEQJmaq8sk3KWD8w="
+                             className={"img-fluid"}
+                             alt="car"/>
+                    </Col>
+                </Row>
+            </Container>
+            {/* content */}
+        </>
+    )
+}
